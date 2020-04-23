9d901d22681588d1989c6671035a5c8175d8a9b6
Manuel Pineyro <manuel.o.pineyro@gmail.com>
2020-04-23 11:44:32 -0400

    flutter aliases

--- flutter/path.zsh
+++ flutter/path.zsh
@@ -1,2 +1,10 @@
 export PATH="$PATH:/usr/local/share/flutter/bin"
 export PATH="$PATH":"/usr/local/share/flutter/.pub-cache/bin"
+
+export GENYMOTION_APP=/Applications/Genymotion.app/Contents/MacOS
+export GENYMOTION_SHELL=/Applications/Genymotion\ Shell.app/Contents/MacOS/
+export GENYMOTION_PLAYER=/Applications/Genymotion.app/Contents/MacOS/player.app/Contents/MacOS/
+
+export PATH=$PATH:$GENYMOTION_APP
+export PATH=$PATH:$GENYMOTION_SHELL
+export PATH=$PATH:$GENYMOTION_PLAYER
